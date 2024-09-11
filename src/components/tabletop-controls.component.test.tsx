829f2a75d5e0a4eac57209418300f07e8bb113d1
Brett Pyke <brett@brettpyke.com>
2024-09-11 14:39:53 +1000

    add keyboard event movement listeners

--- src/components/tabletop-controls.component.test.tsx
+++ src/components/tabletop-controls.component.test.tsx
@@ -3,66 +3,62 @@ import userEvent from '@testing-library/user-event';
 import { TabletopControls } from './tabletop-controls.component';
 import { TabletopGrid } from '../services';
 
+const BUTTON_EVENTS: string[][] = [
+  ['Up', 'up'],
+  ['Down', 'down'],
+  ['Left', 'left'],
+  ['Right', 'right'],
+];
+
+const KEYBOARD_EVENTS: string[][] = [
+  ['{ArrowUp}', 'up'],
+  ['{ArrowDown}', 'down'],
+  ['{ArrowLeft}', 'left'],
+  ['{ArrowRight}', 'right'],
+  // Edge (16 and earlier) and Firefox (36 and earlier)
+  // use the non-standard key names below
+  ['{Up}', 'up'],
+  ['{Down}', 'down'],
+  ['{Left}', 'left'],
+  ['{Right}', 'right'],
+];
+
 describe('Tabletop Controls', () => {
   it('should render the controls', () => {
     render(<TabletopControls grid={new TabletopGrid()} />);
     const buttons = screen.getAllByRole('button');
     expect(buttons, 'controls should render 4 buttons').toHaveLength(4);
   });
-  it('should move "up" when "Up" is clicked', async () => {
-    const user = userEvent.setup();
-    let movedDirection: string | undefined = undefined;
-    const grid: Pick<TabletopGrid, 'move'> = {
-      move: (direction: string) => {
-        movedDirection = direction;
-        return { x: 0, y: 0 };
-      },
-    };
-    render(<TabletopControls grid={grid as TabletopGrid} />);
-    await user.click(screen.getByText('Up'));
-    expect(movedDirection, 'no direction was moved').toBeDefined();
-    expect(movedDirection, 'movement direction was incorrect').toBe('up');
+  BUTTON_EVENTS.forEach((event) => {
+    it(`should move "${event[1]}" when "${event[0]}" is clicked`, async () => {
+      const user = userEvent.setup();
+      let movedDirection: string | undefined = undefined;
+      const grid: Pick<TabletopGrid, 'move'> = {
+        move: (direction: string) => {
+          movedDirection = direction;
+          return { x: 0, y: 0 };
+        },
+      };
+      render(<TabletopControls grid={grid as TabletopGrid} />);
+      await user.click(screen.getByText(event[0]));
+      expect(movedDirection, 'no direction was moved').toBeDefined();
+      expect(movedDirection, 'movement direction was incorrect').toBe(event[1]);
+    });
   });
-  it('should move "down" when "Down" is clicked', async () => {
-    const user = userEvent.setup();
-    let movedDirection: string | undefined = undefined;
-    const grid: Pick<TabletopGrid, 'move'> = {
-      move: (direction: string) => {
-        movedDirection = direction;
-        return { x: 0, y: 0 };
-      },
-    };
-    render(<TabletopControls grid={grid as TabletopGrid} />);
-    await user.click(screen.getByText('Down'));
-    expect(movedDirection, 'no direction was moved').toBeDefined();
-    expect(movedDirection, 'movement direction was incorrect').toBe('down');
-  });
-  it('should move "left" when "Left" is clicked', async () => {
-    const user = userEvent.setup();
-    let movedDirection: string | undefined = undefined;
-    const grid: Pick<TabletopGrid, 'move'> = {
-      move: (direction: string) => {
-        movedDirection = direction;
-        return { x: 0, y: 0 };
-      },
-    };
-    render(<TabletopControls grid={grid as TabletopGrid} />);
-    await user.click(screen.getByText('Left'));
-    expect(movedDirection, 'no direction was moved').toBeDefined();
-    expect(movedDirection, 'movement direction was incorrect').toBe('left');
-  });
-  it('should move "right" when "Right" is clicked', async () => {
-    const user = userEvent.setup();
-    let movedDirection: string | undefined = undefined;
-    const grid: Pick<TabletopGrid, 'move'> = {
-      move: (direction: string) => {
-        movedDirection = direction;
-        return { x: 0, y: 0 };
-      },
-    };
-    render(<TabletopControls grid={grid as TabletopGrid} />);
-    await user.click(screen.getByText('Right'));
-    expect(movedDirection, 'no direction was moved').toBeDefined();
-    expect(movedDirection, 'movement direction was incorrect').toBe('right');
+  KEYBOARD_EVENTS.forEach((event) => {
+    it(`should move "${event[1]}" when "${event[0]}" is typed`, async () => {
+      const user = userEvent.setup();
+      let movedDirection: string | undefined = undefined;
+      const grid: Pick<TabletopGrid, 'move'> = {
+        move: (direction: string) => {
+          movedDirection = direction;
+          return { x: 0, y: 0 };
+        },
+      };
+      render(<TabletopControls grid={grid as TabletopGrid} />);
+      await user.keyboard(event[0]);
+      expect(movedDirection, 'no direction was moved').toBeDefined();
+      expect(movedDirection, 'movement direction was incorrect').toBe(event[1]);
+    });
   });
 });
--- src/components/tabletop-controls.component.tsx
+++ src/components/tabletop-controls.component.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { TabletopGrid } from '../services';
 
 export interface TabletopControlsProperties {
@@ -5,6 +6,32 @@ export interface TabletopControlsProperties {
 }
 
 export function TabletopControls({ grid }: TabletopControlsProperties) {
+  useEffect(() => {
+    const keyboardListener = (event: KeyboardEvent) => {
+      switch (event.key) {
+        case 'Up': // Edge (16 and earlier) and Firefox (36 and earlier)
+        case 'ArrowUp':
+          grid.move('up');
+          break;
+        case 'Down': // Edge (16 and earlier) and Firefox (36 and earlier)
+        case 'ArrowDown':
+          grid.move('down');
+          break;
+        case 'Left': // Edge (16 and earlier) and Firefox (36 and earlier)
+        case 'ArrowLeft':
+          grid.move('left');
+          break;
+        case 'Right': // Edge (16 and earlier) and Firefox (36 and earlier)
+        case 'ArrowRight':
+          grid.move('right');
+          break;
+      }
+    };
+    window.addEventListener('keydown', keyboardListener);
+    return () => {
+      window.removeEventListener('keydown', keyboardListener);
+    };
+  }, [grid]);
   return (
     <>
       <button onClick={() => grid.move('up')}>Up</button>
